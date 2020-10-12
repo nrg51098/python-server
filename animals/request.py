ce7a75ff183ccc4a5e8fda3525b18b6120ec6633
Nikhil Gaikwad <gaikwadrnikhil@gmail.com>
2020-10-11 21:59:06 -0500

    added create animal function, working code chapter 4

--- animals/request.py
+++ animals/request.py
@@ -1,21 +1,21 @@
 ANIMALS = [
     {
         "id": 1,
-        "name": "Snickers",
+        "name": "Animal1",
         "species": "Dog",
         "locationId": 1,
         "customerId": 4
     },
     {
         "id": 2,
-        "name": "Gypsy",
+        "name": "Animal2",
         "species": "Dog",
         "locationId": 1,
         "customerId": 2
     },
     {
         "id": 3,
-        "name": "Blue",
+        "name": "Animal3",
         "species": "Cat",
         "locationId": 2,
         "customerId": 1
@@ -40,3 +40,19 @@ def get_single_animal(id):
             requested_animal = animal
 
     return requested_animal
+
+def create_animal(animal):
+    # Get the id value of the last animal in the list
+    max_id = ANIMALS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the animal dictionary
+    animal["id"] = new_id
+
+    # Add the animal dictionary to the list
+    ANIMALS.append(animal)
+
+    # Return the dictionary with `id` property added
+    return animal
--- locations/request.py
+++ locations/request.py
@@ -1,22 +1,19 @@
 LOCATIONS = [
     {
         "id": 1,
-        "name": "Snickers",
-        "species": "Dog",
+        "name": "Customer1",
         "locationId": 1,
         "customerId": 4
     },
     {
         "id": 2,
-        "name": "Gypsy",
-        "species": "Dog",
+        "name": "Customer2",
         "locationId": 1,
         "customerId": 2
     },
     {
         "id": 3,
-        "name": "Blue",
-        "species": "Cat",
+        "name": "Customer3",
         "locationId": 2,
         "customerId": 1
     }
