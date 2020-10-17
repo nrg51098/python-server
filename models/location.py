f8df86e72ce9ecfd6482e4ea1a588f8ddafb0fdc
Nikhil Gaikwad <gaikwadrnikhil@gmail.com>
2020-10-17 11:06:36 -0500

    finished the chapter 10

--- models/location.py
+++ models/location.py
@@ -3,12 +3,8 @@ class Location():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, name, species, status, location_id, customer_id):
+    def __init__(self, id, name, address):
         self.id = id
         self.name = name
-        self.species = species
-        self.status = status
-        self.location_id = location_id
-        self.customer_id = customer_id
-
-new_location = Location(1, "Snickers", "Dog", "Recreation", 1, 4)
+        self.address = address
+        
